ea25b3949a66da98c5fde7a982d2094fd09d1778
MateusDassolerSpezia <mdspezia@furb.br>
2024-04-08 10:44:19 -0300

    exercicios

--- Unidade4/Projeto4/src/Exe24Uni4.java
+++ Unidade4/Projeto4/src/Exe24Uni4.java
@@ -9,31 +9,59 @@ public class Exe24Uni4 {
         float valor2 = sc.nextFloat();
         float valor3 = sc.nextFloat();
 
+        float maior = 0;
+        float menor = 0;
+        float meio = 0;
+
         System.out.println("Escolha uma opção:");
         System.out.println("1 - escreva os 3 valores em ordem crescente");
         System.out.println("2 - escreva os 3 valores em ordem decrescente");
         System.out.println("3 - escreva os 3 valores de forma que o maior valor fique no meio");
         int opçao = sc.nextInt();
 
-        if (valor1 > valor2 && valor1 > valor3 && valor2 > valor3); {
-            System.out.println(valor1 + ", " + valor2 + ", " +valor3);
-        } else if (valor2 > valor1) {
-            System.out.println(valor2 + ", " + valor1 + ", " +valor3);
+        if (valor1 > valor2 && valor1 > valor3) {
+            maior = valor1;
+        } else if (valor2 > valor3) {
+            maior = valor2;
+        } else {
+            maior = valor3;
+        } 
+
+        if (valor1 < valor2 && valor1 < valor3) {
+            menor = valor1;
+        } else if (valor2 < valor3) {
+            menor = valor2;
+        } else {
+            menor = valor3;
+        }     
+
+        if (valor1 != maior && valor1 != menor) {
+            meio = valor1;
+        } else if (valor2 != maior && valor2 != menor) {
+            meio = valor2;
+        } else {
+            meio = valor3;
         }
 
-        /*switch (opçao) {
+        switch (opçao) {
             case 1:
-            System.out.println("Entre com 3 valores: ");
-            float valor1 = sc.nextFloat();
-            float valor2 = sc.nextFloat();
-            float valor3 = sc.nextFloat();
-            if (valor1 > valor2 && valor1 > valor3 && valor2 > valor3); {
-                System.out.println(valor1 + ", " + valor2 + ", " +valor3);
-            } else if (valor2 > valor1 && valor2 > valor3 && valor1 > valor3); {
-                System.out.println(valor2 + ", " + valor1 + ", " +valor3);
-            }
-        }/* */
+                System.out.println("Ordem crescente:");
+                System.out.println(menor + " " + meio + " " + maior);
+                    break;
+            case 2:
+                System.out.println("Ordem decrescente:");
+                System.out.println(maior + " " + meio + " " + menor);
+                    break;   
+            case 3:
+                System.out.println("Maior no meio:");
+                System.out.println(menor + " " + maior + " " + meio);
+                    break;
+            default: System.out.println("Opção invalida");
+        }
+
+        
         sc.close();
+        
     }
     public static void main(String[] args) {
         new Exe24Uni4();
--- Unidade4/Projeto4/src/Exe27Uni4.java
+++ Unidade4/Projeto4/src/Exe27Uni4.java
@@ -18,9 +18,12 @@ public class Exe27Uni4 {
         System.out.print("Minuto(s): ");
         int minutosSaida = sc.nextInt();
 
+
         int horasEstacionado = (horasSaida - horasChegada);
         int minutosEstacionado = (minutosSaida - minutosChegada);
         System.out.println("tempo estacionado: " + horasEstacionado + "h" + minutosEstacionado);
+
+        float valorPagar = 0;
 
         /*if (horasEstacionado <= 1 && minutosEstacionado < 30) {
             System.out.println("Preço a pagar: R$ 5,00");
@@ -44,10 +47,43 @@ public class Exe27Uni4 {
             System.out.println("Preço a pagar: R$ " + df.format((horasEstacionado + 1) * 10));
         } /* */
 
+        if (minutosEstacionado >= 30) {
+            horasEstacionado += 1;
+        } 
+        
+        if (horasEstacionado == 0 || horasEstacionado == 1) {
+            valorPagar = 5;
+        } else if (horasEstacionado == 2) {
+            valorPagar = 10;
+        } else if (horasEstacionado == 3) {
+            valorPagar = 22.5f;
+        } else if (horasEstacionado == 4){
+            valorPagar = 30;
+        } else if (horasEstacionado >= 5) {
+            valorPagar = horasEstacionado * 10;
+        } 
+
         switch (horasEstacionado) {
-            case 1: 
+            case 0: 
+            case 1:
+                valorPagar = 5;
+                break;
+            case 2:
+                valorPagar = 10;
+                break;
+            case 3:
+                valorPagar = 22.5f;
+                break;
+            case 4: 
+                valorPagar = 30;
+                break;
+            default: //>=5
+                int horasMais = horasEstacionado - 4;
+                valorPagar = 30 + (10 * horasMais);
+                break;
         }
 
+        System.out.println("Preço a pagar: R$ " + df.format(valorPagar));
 
         sc.close();
     }
