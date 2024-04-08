d8e49f78d475692e5ebe619653b05086a0f74458
MateusDassolerSpezia <mdspezia@furb.br>
2024-04-08 11:12:55 -0300

    mudanças

---
+++ AtividadeUni4Uri/src/Exe2Uni4Uri.java
@@ -0,0 +1,8 @@
+public class Exe2Uni4Uri {
+    public Exe2Uni4Uri() {
+        
+    }
+    public static void main(String[] args) {
+        new Exe2Uni4Uri();
+    }
+}
--- AtividadeUriUni4/AtividadeUni4Uri/src/Exe1Uni4Uri.java
+++
@@ -1,8 +0,0 @@
-public class Exe1Uni4Uri {
-    public Exe1Uni4Uri() {
-        
-    }
-    public static void main(String[] args) {
-        new Exe1Uni4Uri();
-    }
-}
