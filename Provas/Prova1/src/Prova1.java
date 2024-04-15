c23dc73d3294ac8a8c2b8b6f25becbd756bdee9e
MateusDassolerSpezia <mdspezia@furb.br>
2024-04-15 10:34:02 -0300

    Projeto Prova1

---
+++ Provas/Prova1/src/Prova1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prova1 {
+    public Prova1() {
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+    public static void main(String[] args) {
+        new Prova1();
+    }
+}
